d9efc4ff5f93e03ff295e2795eecd61215a08f53
Jasper <jasperdoan@gmail.com>
2024-10-21 11:09:01 -0700

    init multithreading branch

--- launch.py
+++ launch.py
@@ -1,19 +1,25 @@
 from configparser import ConfigParser
 from argparse import ArgumentParser
-
 from utils.server_registration import get_cache_server
 from utils.config import Config
-from crawler import Crawler
-
+from crawler.frontier import Frontier
+from crawler.worker import Worker
 
 def main(config_file, restart):
     cparser = ConfigParser()
     cparser.read(config_file)
     config = Config(cparser)
     config.cache_server = get_cache_server(config, restart)
-    crawler = Crawler(config, restart)
-    crawler.start()
-
+    frontier = Frontier(config, restart)
+    
+    workers = []
+    for i in range(config.threads_count):
+        worker = Worker(i, config, frontier)
+        worker.start()
+        workers.append(worker)
+    
+    for worker in workers:
+        worker.join()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
