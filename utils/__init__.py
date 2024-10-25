8e5d2918f2c68c3dd8eb14497be097b7e4d81d87
Jasper <jasperdoan@gmail.com>
2024-10-25 11:46:56 -0700

    Refactor logging to include elapsed time in seconds

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,19 @@
 import os
+import time
 import logging
+
 from hashlib import sha256
 from urllib.parse import urlparse
+
+class ElapsedTimeFormatter(logging.Formatter):
+    def __init__(self, fmt=None, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+        self.start_time = time.time()
+
+    def formatTime(self, record, datefmt=None):
+        elapsed_seconds = record.created - self.start_time
+        return f"{elapsed_seconds:.3f}"
+
 
 def get_logger(name, filename=None):
     logger = logging.getLogger(name)
@@ -12,9 +24,8 @@ def get_logger(name, filename=None):
     fh.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-       "%(asctime)s.%(msecs)03d | %(name)s | %(message)s",
-       datefmt='%H:%M:%S')
+    formatter = ElapsedTimeFormatter(
+       "%(asctime)s s | %(name)s | %(message)s")
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
     # add the handlers to the logger
